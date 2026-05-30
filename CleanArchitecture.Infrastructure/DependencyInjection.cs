ec7377dc69998e5a67b91a077df997971169ada9
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-05-30 18:10:18 +0530

    .

--- CleanArchitecture.Infrastructure/DependencyInjection.cs
+++ CleanArchitecture.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,16 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecture.Infrastructure
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection ImplementPersistence(
+            IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MovieRentalDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(MovieRentalDbContext).Assembly.FullName)), ServiceLifetime.Transient);
+
+            services.AddScoped<IMovieRentalDbContext>(provider => provider.GetService<MovieRentalDbContext>());
+            return services;
+        }
     }
 }
